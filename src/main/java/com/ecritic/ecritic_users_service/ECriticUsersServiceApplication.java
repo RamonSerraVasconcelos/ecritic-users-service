245bdb5f5edccbcb3280bd04fff40ce34084aabe
Ramon Vasconcelos <rserravasconcelos@gmail.com>
2024-05-25 16:49:34 -0300

    Adding project files

---
+++ src/main/java/com/ecritic/ecritic_users_service/ECriticUsersServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ecritic.ecritic_users_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECriticUsersServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECriticUsersServiceApplication.class, args);
+	}
+
+}
