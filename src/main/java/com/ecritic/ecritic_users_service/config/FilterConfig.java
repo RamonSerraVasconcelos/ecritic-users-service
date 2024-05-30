dd573c0a4ffb021eee101de4976b412b473c5594
Ramon Vasconcelos <rserravasconcelos@gmail.com>
2024-05-29 23:42:31 -0300

    Creating request filter

---
+++ src/main/java/com/ecritic/ecritic_users_service/config/FilterConfig.java
@@ -0,0 +1,26 @@
+package com.ecritic.ecritic_users_service.config;
+
+import com.ecritic.ecritic_users_service.entrypoint.validation.ApplicationRequestFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class FilterConfig {
+
+    private final ApplicationRequestFilter applicationRequestFilter;
+
+    @Bean
+    public FilterRegistrationBean<ApplicationRequestFilter> authenticationFilter() {
+
+        FilterRegistrationBean<ApplicationRequestFilter> registrationBean = new FilterRegistrationBean<>();
+
+        registrationBean.setFilter(applicationRequestFilter);
+        registrationBean.addUrlPatterns("*");
+        registrationBean.addInitParameter("excludeUrls", "/users,/forgot-password,/reset-password,/reset-email,/countries,/countries/*");
+
+        return registrationBean;
+    }
+}
---
+++ src/main/java/com/ecritic/ecritic_users_service/entrypoint/validation/RequestHeaders.java
@@ -0,0 +1,15 @@
+package com.ecritic.ecritic_users_service.entrypoint.validation;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Builder
+@Setter
+@Getter
+@ToString
+public class RequestHeaders {
+
+    private String requestId;
+}
