3500c6b36e0ee314d90da67a59d5aa5e72085351
Ramon Vasconcelos <rserravasconcelos@gmail.com>
2024-05-30 01:18:19 -0300

    Creating endpoint and business logic to update user

---
+++ src/main/java/com/ecritic/ecritic_users_service/core/usecase/UpdateUserUseCase.java
@@ -0,0 +1,52 @@
+package com.ecritic.ecritic_users_service.core.usecase;
+
+import com.ecritic.ecritic_users_service.core.model.Country;
+import com.ecritic.ecritic_users_service.core.model.User;
+import com.ecritic.ecritic_users_service.core.usecase.boundary.FindCountryByIdBoundary;
+import com.ecritic.ecritic_users_service.core.usecase.boundary.FindUserByIdBoundary;
+import com.ecritic.ecritic_users_service.core.usecase.boundary.SaveUserBoundary;
+import com.ecritic.ecritic_users_service.exception.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class UpdateUserUseCase {
+
+    private final SaveUserBoundary saveUserBoundary;
+
+    private final FindUserByIdBoundary findUserByIdBoundary;
+
+    private final FindCountryByIdBoundary findCountryByIdBoundary;
+
+    public User execute(UUID userId, User userRequest) {
+        log.info("Updating user with id: [{}]", userRequest.getId());
+
+        try {
+            Optional<User> userToBeUpdatedOptional = findUserByIdBoundary.execute(userId);
+
+            if(userToBeUpdatedOptional.isEmpty()) {
+                throw new EntityNotFoundException("User not found");
+            }
+
+            Country country = findCountryByIdBoundary.execute(userRequest.getCountry().getId());
+
+            User userToBeUpdated = userToBeUpdatedOptional.get();
+
+            userToBeUpdated.setName(userRequest.getName());
+            userToBeUpdated.setDescription(userRequest.getDescription());
+            userToBeUpdated.setPhone(userRequest.getPhone());
+            userToBeUpdated.setCountry(country);
+
+            return saveUserBoundary.execute(userToBeUpdated);
+        } catch (Exception ex) {
+            log.error("Error while updating user with id: [{}]", userId, ex);
+            throw ex;
+        }
+    }
+}
